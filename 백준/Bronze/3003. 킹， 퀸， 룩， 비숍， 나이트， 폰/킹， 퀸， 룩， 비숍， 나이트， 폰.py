6b698577c6b8682dd88a3d7e45b2a513ba2ce87e
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-07-19 16:36:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.py
@@ -1,26 +1,3 @@
 a,b,c,d,e,f = map(int, input().split())
-if 1-a != 0:
-    a = 1-a
-else:
-    a = 0
-if 1-b != 0:
-    b = 1-b
-else:
-    b = 0
-if 2-c != 0:
-    c = 2-c
-else:
-    c = 0
-if 2-d != 0:
-    d = 2-d
-else:
-    d = 0
-if 2-e != 0:
-    e = 2-e
-else:
-    e = 0
-if 8-f != 0:
-    f = 8-f
-else:
-    f = 0
-print(a,b,c,d,e,f)
+print(1-a, 1-b, 2-c, 2-d, 2-e, 8-f)
+ 
