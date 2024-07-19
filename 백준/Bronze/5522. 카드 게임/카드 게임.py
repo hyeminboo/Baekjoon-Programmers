9aefe6cbd88dba2b8700cf0ff6686bfda4a2d133
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-07-19 16:40:15 +0900

    [Bronze V] Title: 카드 게임, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5522. 카드 게임/카드 게임.py
+++ 백준/Bronze/5522. 카드 게임/카드 게임.py
@@ -1,6 +1,4 @@
-num = []
-while len(num) < 5:
-    a = int(input())
-    if a >= 0 and a <= 10:
-        num.append(a)
-print(sum(num))
+score = []
+for i in range(5):
+    score.append(int(input()))
+print(sum(score))
