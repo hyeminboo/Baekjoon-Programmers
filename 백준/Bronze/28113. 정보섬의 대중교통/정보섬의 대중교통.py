78c2db0242d4510b6e9a27f8f64b45ffaac55cfb
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-07-25 20:43:36 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.py
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.py
@@ -1,8 +1,8 @@
 n, a, b = map(int, input().split())
-if n <= b:
-    if a < b:
-        print("Bus")
-    elif a == b:
-        print("Anything")
-    else:
-        print("Subway")
+
+if a < b:
+    print("Bus")
+elif a == b:
+    print("Anything")
+else:
+    print("Subway")
