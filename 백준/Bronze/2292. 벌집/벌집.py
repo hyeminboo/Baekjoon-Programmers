a53b7e3e4aea4f8585dbc0988d5076cc7bb4766c
hyemin <139746386+hyeminboo@users.noreply.github.com>
2024-08-11 14:41:13 +0900

    [Bronze II] Title: 벌집, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2292. 벌집/벌집.py
+++ 백준/Bronze/2292. 벌집/벌집.py
@@ -1,14 +1,8 @@
 n = int(input())
 i = 1
-num = 1
+m = 1
 
-while True:
-    if n == 1:
-        print(1)
-        break
-    else:
-        num += 6 * i
-        if num >= n:
-            print(i+1)
-            break
-        i += 1
+while n > m:
+    m += 6 * i
+    i += 1
+print(i)
